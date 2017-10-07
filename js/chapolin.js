d3eabc626dba446794df12262eae96ed32d111e6
Rafael Faria <rafaelr.faria@yahoo.com.br>
2017-10-07 18:16:16 -0300

    ajuste do botao e adicionado função de clipboard

--- js/chapolin.js
+++ js/chapolin.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {	
-	
+$(document).ready(function() {
+
 	var chapolin = new Array();
 	chapolin[0] = "Quem era ele? Era o Super San. O que é isso? É uma espécie de Chapolin Colorado mas com conta bancária.";
 	chapolin[1] = "Vai me dizer que acredita em bruxas? Mas é claro que não, mas que elas existem, existem.";
@@ -9,25 +9,33 @@ $(document).ready(function() {
 	chapolin[5] = "Não poderei comemorar meu aniversário em setembro este ano. Por que? Porque nasci em março.";
 	chapolin[6] = "Ele rico, eu pobre. Ele era inquieto, eu tranquilo. Ele era preguiçoso, eu estudioso. Eu era Pacífico, ele Atlântico.";
 	chapolin[7] = "Não contavam com minha astúcia!";
-	
+
 	function geraFraseAleatoria(array){
 		var fraseAleatoria = chapolin[Math.floor(Math.random() * chapolin.length)];
 		return fraseAleatoria;
-	}	
-	
-	$('.botao').click(function(){ 
-		var quantidade = $('.quantidade-frases').data('quantidade');
+	}
+
+	$('.gerar_chapolin').click(function(){
+		var quantidade = $('.quantidade-frases.active').data('quantidade');
 		var frases = "";
 		for(i = 0; i < quantidade; i++) {
-		 frases += geraFraseAleatoria();
+			frases +="\t"+geraFraseAleatoria()+"\n";
 		}
 		$('.chapolin').val(frases);
-	});	
-	
-	$('.gerar_chapolin').zclip({
-		path: "js/ZeroClipboard.swf",
-		copy: function(){
-			return $(this).prev().val();
-		}
-	});	
+		copyToClipboard($('.chapolin').val());
+	});
+
+	$('.quantidade-frases').click(function() {
+		$('.quantidade-frases').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	function copyToClipboard(value) {
+		var aux = document.createElement("input");
+		aux.setAttribute("value", value);
+		document.body.appendChild(aux);
+		aux.select();
+		document.execCommand("copy");
+		document.body.removeChild(aux);
+	}
 });
